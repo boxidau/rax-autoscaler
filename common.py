cf60162fa1aa33aa9f734cf4e020894dae2fb972
Simon Mirco <simon@box.id.au>
2014-09-04 08:32:05 +0000

    Enhancement: #3 token now cached for re-use

--- common.py
+++ common.py
@@ -4,6 +4,8 @@ import pyrax.exceptions as pexc
 from termcolor import colored
 import ConfigParser
 import subprocess
+import datetime
+import json
 
 path = os.path.dirname(os.path.realpath(__file__))
 config_file = path + "/config.ini"
@@ -20,14 +22,42 @@ def log(level, message):
 
 def authenticate(dc):
   pyrax.set_setting("identity_type", "rackspace")
-
-  log("INFO", "Authenticating")
-
-  try:
-    pyrax.set_credential_file(config_file, region=dc)
-  except pexc.AuthenticationFailed:
-    log('ERROR', 'Authentication Failure')
   
+  try: 
+    log("INFO", "Authenticating with cached token")
+    token_file = open(path + '/.token_cache', 'r')
+    identity = json.load(token_file)
+    token_file.close()
+
+    expiry = datetime.datetime.strptime(identity['expires'], '%Y-%m-%d %H:%M:%S')
+    present = datetime.datetime.now()
+    expires_in = expiry - present
+    if expires_in.total_seconds() < 600:
+      log('INFO', 'Token expires soon or is already expired, re-authenticating')
+      raise Exception('Token Expired')
+
+    pyrax.auth_with_token(identity['token'], tenant_id=identity['tenant_id'], tenant_name=identity['username'])
+    pyrax.identity.region = dc
+
+  except:
+    log("INFO", "Authenticating with API Key")
+    
+    try:
+      pyrax.set_credential_file(config_file, region=dc)
+     
+      try: 
+        token_file = open(path + '/.token_cache', 'w')
+        identity = {}
+        for attr in ['token', 'tenant_id', 'username', 'region']:
+          identity[attr] = getattr(pyrax.identity, attr)
+  
+        identity['expires'] = str(pyrax.identity.expires)
+        json.dump(identity, token_file, indent=4)
+      except:
+        log('ERROR', 'Unable to write token cache')
+    except pexc.AuthenticationFailed:
+      log('ERROR', 'Authentication Failure')
+    
   log('OK', 'Authentication Successful')
 
 def get_config(group):
