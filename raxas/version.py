80910c1c808ce1b7fbb775e89dcf9dbbf31373d3
Simone Soldateschi <simone.soldateschi@gmail.com>
2014-11-14 13:55:52 +1100

    Bump version: 0.1.0 → 0.1.1

--- raxas/version.py
+++ raxas/version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 def return_version():
   return "rax-autoscaler v%s -- Copyright @ 2014 Rackspace" % VERSION
