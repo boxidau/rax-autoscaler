7e907eedb7a580b92aaa7af456926472f62e0716
jon-walton <jonwalton@gmail.com>
2014-12-09 10:57:02 +0800

    fix python 2.6 compatibility

--- tests/test_common.py
+++ tests/test_common.py
@@ -35,11 +35,11 @@ class CommonTest(unittest.TestCase):
         self._config_json = """
         {
             "auth": {
-                    "os_username": "api_username",
-                    "os_password": "api_key",
-                    "os_region_name": "os_region_name"
-                },
-                "autoscale_groups": {
+                "os_username": "api_username",
+                "os_password": "api_key",
+                "os_region_name": "os_region_name"
+            },
+            "autoscale_groups": {
                 "group0": {
                     "group_id": "group id",
                     "scale_up_policy": "scale up policy id",
@@ -52,20 +52,20 @@ class CommonTest(unittest.TestCase):
                     "webhooks": {
                         "scale_up": {
                             "pre": [
-                                "url",
-                                "url"
+                                "preup1",
+                                "preup2"
                             ],
                             "post": [
-                                "url"
+                                "postup1"
                             ]
                         },
                         "scale_down": {
                             "pre": [
-                                "url",
-                                "url"
+                                "predwn1",
+                                "predwn2"
                             ],
                             "post": [
-                                "url"
+                                "postdwn1"
                             ]
                         }
                     }
@@ -76,81 +76,87 @@ class CommonTest(unittest.TestCase):
 
         super(CommonTest, self).__init__(*args, **kwargs)
 
-    def test_check_file_should_return_abs_path(self):
-        with patch('os.path.isfile', return_value=True) as isfile_mock, \
-                patch('os.access', return_value=True) as access_mock:
-            check_file = '/config.cfg'
+    @patch('os.path.isfile', return_value=True)
+    @patch('os.access', return_value=True)
+    def test_check_file_should_return_abs_path(self, access_mock, isfile_mock):
+        check_file = '/config.cfg'
 
-            self.assertEqual(common.check_file(check_file), check_file)
-            isfile_mock.assert_called_once_with(check_file)
-            access_mock.assert_called_once_with(check_file, os.R_OK)
+        self.assertEqual(common.check_file(check_file), check_file)
+        isfile_mock.assert_called_once_with(check_file)
+        access_mock.assert_called_once_with(check_file, os.R_OK)
 
-    def test_check_file_should_return_none(self):
-        with patch('os.path.isfile', return_value=False) as isfile_mock:
-            check_file = 'config.cfg'
+    @patch('os.path.isfile', return_value=False)
+    def test_check_file_should_return_none(self, isfile_mock):
+        check_file = 'config.cfg'
 
-            self.assertIsNone(common.check_file(check_file))
-            isfile_mock.assert_called_with('/etc/rax-autoscaler/config.cfg')
+        self.assertIsNone(common.check_file(check_file))
+        isfile_mock.assert_called_with('/etc/rax-autoscaler/config.cfg')
 
     def test_get_config_should_match(self):
-        with patch('raxas.common.open',
-                   mock_open(read_data=self._config_json), create=True) \
-                as open_mock:
+        with patch('__builtin__.open',
+                   mock_open(read_data=self._config_json)) as open_mock:
 
             self.assertEqual(common.get_config('config.cfg'),
                              json.loads(self._config_json))
             open_mock.assert_called_once_with('config.cfg')
 
     def test_get_config_should_return_none_with_invalid_json(self):
-        with patch('raxas.common.open',
-                   mock_open(read_data='trcfucbujnmk+'), create=True) \
-                as open_mock:
+        with patch('__builtin__.open',
+                   mock_open(read_data='trcfucbujnmk+')) as open_mock:
 
             self.assertIsNone(common.get_config('config.cfg'))
             open_mock.assert_called_once_with('config.cfg')
 
-    def test_get_machine_uuid(self):
+    @patch('raxas.common.subprocess')
+    def test_get_machine_uuid(self, subprocess_mock):
         for i in xrange(0, 5):
             current_uuid = str(uuid.uuid4())
-            with patch('raxas.common.subprocess') as subprocess_mock:
-                subprocess_mock.Popen.return_value.communicate.\
-                    return_value = ['instance-%s\n' % current_uuid]
+            subprocess_mock.Popen.return_value.communicate.\
+                return_value = ['instance-%s\n' % current_uuid]
 
-                self.assertEqual(common.get_machine_uuid(), current_uuid)
+            self.assertEqual(common.get_machine_uuid(), current_uuid)
 
-    def test_get_user_value_from_config(self):
-        with patch('sys.argv',
-                   return_value=['/opt/pycharm/helpers/pycharm/noserunner.py',
-                                 '/home/github/rax-autoscaler/tests/']):
-            args = parse_args()
-            self.assertEqual(
-                common.get_user_value(args, json.loads(self._config_json),
-                                      'os_username'), 'api_username')
-            self.assertEqual(
-                common.get_user_value(args, json.loads(self._config_json),
-                                      'os_password'), 'api_key')
-            self.assertEqual(
-                common.get_user_value(args, json.loads(self._config_json),
-                                      'os_region_name'), 'os_region_name')
+    @patch('sys.argv', return_value=['/path/to/noserunner.py', '/home/tests/'])
+    def test_get_user_value_from_config(self, argv_mock):
+        args = parse_args()
+        self.assertEqual(
+            common.get_user_value(args, json.loads(self._config_json),
+                                  'os_username'), 'api_username')
+        self.assertEqual(
+            common.get_user_value(args, json.loads(self._config_json),
+                                  'os_password'), 'api_key')
+        self.assertEqual(
+            common.get_user_value(args, json.loads(self._config_json),
+                                  'os_region_name'), 'os_region_name')
 
-            with self.assertRaises(KeyError):
-                common.get_user_value(args, json.loads(self._config_json),
-                                      'should raise KeyError')
+        with self.assertRaises(KeyError):
+            common.get_user_value(args, json.loads(self._config_json),
+                                  'should raise KeyError')
 
     def test_get_group_value(self):
-        self.assertEqual(common.get_group_value(json.loads(self._config_json),
-                                                'group0', 'group_id'),
+        config = json.loads(self._config_json)
+        self.assertEqual(common.get_group_value(config, 'group0', 'group_id'),
                          'group id')
-        self.assertEqual(common.get_group_value(json.loads(self._config_json),
-                                                'group0', 'scale_up_policy'),
+        self.assertEqual(common.get_group_value(config, 'group0',
+                                                'scale_up_policy'),
                          'scale up policy id')
-        self.assertEqual(common.get_group_value(json.loads(self._config_json),
-                                                'group0', 'check_config'),
-                         {})
+        self.assertEqual(common.get_group_value(config, 'group0',
+                                                'check_config'), {})
 
-        self.assertIsNone(
-            common.get_group_value(json.loads(self._config_json),
-                                   'group0', 'should raise KeyError'))
+        self.assertIsNone(common.get_group_value(config, 'group0',
+                                                 'should raise KeyError'))
+
+    def test_get_webhook_value(self):
+        config = json.loads(self._config_json)
+        self.assertEqual(common.get_webhook_value(config, 'group0',
+                                                  'scale_up'),
+                         {'post': ['postup1'], 'pre': ['preup1', 'preup2']})
+        self.assertEqual(common.get_webhook_value(config, 'group0',
+                                                  'scale_down'),
+                         {'post': ['postdwn1'], 'pre': ['predwn1', 'predwn2']})
+
+        self.assertIsNone(common.get_webhook_value(config, 'group0',
+                                                   'should raise KeyError'))
 
 if __name__ == '__main__':
     unittest.main()
